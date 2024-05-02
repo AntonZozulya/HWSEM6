e3f2dc34f1efb3e8ce805e882ce463e7f487e55a
AntonZozulya <antonzo93@mail.ru>
2024-05-02 20:49:56 +0300

    add controller

---
+++ Controller.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class Controller {
+    private StreamService streamService;
+
+    public Controller() {
+        this.streamService = new StreamService();
+    }
+
+    // Изменяем тип параметра на List<? extends Iterable<StudentGroup>>
+    public void sortStreams(List<? extends Iterable<StudentGroup>> iterableStreams) {
+        streamService.sortStreams(iterableStreams);
+    }
+}
