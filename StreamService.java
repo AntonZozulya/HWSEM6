f8a9758e0257733db2de81b2b23c178a8fcec109
AntonZozulya <antonzo93@mail.ru>
2024-05-02 21:00:32 +0300

    add StreamService

---
+++ StreamService.java
@@ -0,0 +1,17 @@
+import java.util.Collections;
+import java.util.List;
+
+public class StreamService {
+    // Изменяем тип параметра на List<? extends Iterable<StudentGroup>>
+    public void sortStreams(List<? extends Iterable<StudentGroup>> iterableStreams) {
+        Collections.sort(iterableStreams, (s1, s2) -> Integer.compare(countGroups(s1), countGroups(s2)));
+    }
+
+    private int countGroups(Iterable<StudentGroup> iterable) {
+        int count = 0;
+        for (StudentGroup group : iterable) {
+            count++;
+        }
+        return count;
+    }
+}
